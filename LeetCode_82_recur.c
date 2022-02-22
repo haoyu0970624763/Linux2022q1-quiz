043a4f5ec388e98d3e4c0bb2962fa43a45c1b44a
haoyu0970624763 <haoyu0970624763@gmail.com>
2022-02-22 19:30:00 +0800

    Fix memory leak problem

--- LeetCode_82_recur.c
+++ LeetCode_82_recur.c
@@ -14,7 +14,7 @@ struct ListNode *deleteDuplicates(struct ListNode *head)
     /* node is not empty*/
     /* this node->val is equal to node->next->val */
     if (head->next && head->val==head->next->val) {
-
+        
         /* check there are more than one repeated number equal to head node */
         while (head->next && head->val==head->next->val){
             struct ListNode *tmp=head;
@@ -22,9 +22,11 @@ struct ListNode *deleteDuplicates(struct ListNode *head)
             /* free the memory allocated to the duplicate  node */
             free(tmp);
         }
-        
+        struct ListNode *tmp=head;
+        head = head -> next;
+        free(tmp);
         // return the node which val is not equal to head node
-        return deleteDuplicates(head->next);
+        return deleteDuplicates(head);
     }
     /* 
         this node->val is not equal to node->next->val 
