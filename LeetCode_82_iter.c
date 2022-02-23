c6cb18e4dbbbe698865498187bffb634598da716
haoyu0970624763 <haoyu0970624763@gmail.com>
2022-02-23 16:44:43 +0800

    Add the annotation of the code

--- LeetCode_82_iter.c
+++ LeetCode_82_iter.c
@@ -8,8 +8,13 @@ struct ListNode
 
 struct ListNode *deleteDuplicates(struct ListNode *head)
 {
-    
 
+    if (!head)
+    {
+        return NULL;
+    }
+
+    /* Deal with the duplicate case appear in head */
     while (head)
     {
         if (head->next && head->val == head->next->val)
@@ -27,35 +32,48 @@ struct ListNode *deleteDuplicates(struct ListNode *head)
         }
         else
         {
+            /* if no duplicate case appear in head , then out of the while loop*/
             break;
         }
     }
 
-    struct ListNode *tmp_head = head;
-    
-
-    while (head)
+    if (!head)
     {
-        if (head->next && head->next->next && head->next->val == head->next->next->val)
+        return NULL;
+    }
+    /* use current to traverse the list */
+    struct ListNode *current = head;
+
+    /* In here , first node will definitely not equal to second node , if there has second node */
+    /* if list have just one node , we just return it , otherwise we deal with it */
+    while (current->next)
+    {
+        /*
+            Use variable prev to record now we traverse
+            and simply check two nodes behind it are duplicate or not
+
+            if two nodes behind it are duplicate , deal with it
+            otherwise just traverse next node
+        */
+        if (current->next->next && current->next->val == current->next->next->val)
         {
-            struct ListNode *tmp = head;
-            
-            int same_value = head->next->val;
-            head=head->next;
-            while ( head && head->val == same_value)
+            struct ListNode *prev = current;
+            int same_value = current->next->val;
+            current = current->next;
+            while (current && current->val == same_value)
             {
-                head=head->next;
+                struct ListNode *tmp = current;
+                current = current->next;
                 /* free the memory allocated to the duplicate  node */
+                free(tmp);
             }
-            tmp-> next  = head ; 
-            head=tmp;
-            
+            prev->next = current;
+            current = prev;
         }
         else
         {
-            head=head->next;
+            current = current->next;
         }
     }
-
-    return tmp_head;
+    return head;
 }
